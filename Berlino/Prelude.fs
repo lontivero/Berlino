aec2c9dd77d2d18ce124786a8f714492e9ac5d3b
Lucas Ontivero <lucasontivero@gmail.com>
2023-07-28 21:03:18 -0300

    Derive scriptPubKeys

--- Berlino/Prelude.fs
+++ Berlino/Prelude.fs
@@ -1,6 +1,21 @@
 namespace Berlino
 
+open System
+open System.Collections.Generic
+open System.Linq
+
 [<AutoOpen>]
 module Utils =
     let curry f a b = f (a,b)
     let uncurry f (a,b) = f a b
+
+    let memoize f =
+        let dict = Dictionary<_, _>()
+        fun c ->
+            let exists, value = dict.TryGetValue c
+            match exists with
+            | true -> value
+            | _ ->
+                let value = f c
+                dict.Add(c, value)
+                value
--- Berlino/Wallet.fs
+++ Berlino/Wallet.fs
@@ -2,7 +2,7 @@
 
 open System
 open NBitcoin
-       
+
 module Wallet =
 
     module ScriptPubKeyDescriptor =
@@ -10,6 +10,12 @@ module Wallet =
             | Receiving
             | Change
             | CoinJoin
+
+        let purposeIndex =
+            function
+            | Receiving -> 0u
+            | Change -> 1u
+            | CoinJoin -> 2u
 
         type ScriptPubKeyDescriptor = {
             ExtPubKey: ExtPubKey
@@ -20,13 +26,50 @@ module Wallet =
             Gap : uint
         }
 
+        type ScriptPubKeyInfo = {
+            ScriptPubKey : Script
+            Index : uint
+            Generator: ScriptPubKeyDescriptor
+            KeyPath : KeyPath
+        }
+
         let create (extPubKey : ExtPubKey) (fingerprint : HDFingerprint) (keyPath : KeyPath) scriptType scriptPurpose minGapLimit =
-            {        
+            {
                 ExtPubKey = extPubKey
-                Fingerprint = fingerprint 
+                Fingerprint = fingerprint
                 ScriptType = scriptType
                 Purpose = scriptPurpose
-                KeyPath = keyPath 
+                KeyPath = keyPath
                 Gap = minGapLimit
-            }   
-            
+            }
+
+        let getScriptPubKey scriptType (pubkey : PubKey) =
+            match scriptType with
+            | ScriptType.P2WPKH -> pubkey.GetScriptPubKey(ScriptPubKeyType.Segwit)
+            | ScriptType.Taproot -> pubkey.GetScriptPubKey(ScriptPubKeyType.TaprootBIP86)
+            | ScriptType.P2PKH -> pubkey.GetScriptPubKey(ScriptPubKeyType.Legacy)
+            | _ -> failwith $"Unknown script type {scriptType}"
+
+        let deriveExtPubKeyForPurpose = memoize (
+            fun (extPubKey : ExtPubKey, purpose) ->
+                purpose |> purposeIndex |> extPubKey.Derive)
+
+        let derive = memoize (
+            fun (sg : ScriptPubKeyDescriptor, i : uint) ->
+                deriveExtPubKeyForPurpose (sg.ExtPubKey, sg.Purpose)
+                |> fun extPubKey -> extPubKey.Derive(i).PubKey
+                |> getScriptPubKey sg.ScriptType
+                |> fun scp -> {
+                    ScriptPubKey = scp
+                    Index = i
+                    Generator = sg
+                    KeyPath = sg.KeyPath.Derive(purposeIndex sg.Purpose).Derive(i)
+                    })
+
+        let deriveRange sg start count =
+            seq {
+                for i in start .. start + count - 1u do
+                yield derive (sg, i)
+            }
+
+
