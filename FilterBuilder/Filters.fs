05f8f4c9a195ead1d0ec3cca587dd25f49c218c2
Lucas Ontivero <lucasontivero@gmail.com>
2023-07-30 22:32:24 -0300

    taproot activation data for filters

---
+++ FilterBuilder/Filters.fs
@@ -0,0 +1,24 @@
+module Filters
+
+    open NBitcoin
+
+    module Building =
+
+        let taprootActivation (network : Network) =
+            match network.Name with
+            | "Main" -> {|
+                Hash = uint256.Parse "0000000000000000000687bca986194dc2c1f949318629b44bb54ec0a94d8244"
+                PrevBlockHash = uint256.Parse "000000000000000000013712fc242ee6dd28476d0e9c931c75f83e6974c6bccc"
+                Height = 709632
+                |}
+            | "TestNet" -> {|
+                Hash = uint256.Parse "00000000000000216dc4eb2bd27764891ec0c961b0da7562fe63678e164d62a0"
+                PrevBlockHash = uint256.Parse "0000000000000001e17cc7358ee658affcb0a23146176581a7606a15f73993e3"
+                Height = 2007000
+                |}
+            | "RegTest" -> {|
+                Hash = Network.RegTest.GenesisHash
+                PrevBlockHash = Network.RegTest.GetGenesis().Header.HashPrevBlock
+                Height = 0
+                |}
+            | _ -> failwith $"Unknown network '{network.Name}'"
