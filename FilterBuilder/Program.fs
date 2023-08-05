059ed8faae64ee0e7d9f6ef67e09a1063186aeb8
Lucas Ontivero <lucasontivero@gmail.com>
2023-08-04 22:41:29 -0300

    First FilterBuilder working version

--- FilterBuilder/Program.fs
+++ FilterBuilder/Program.fs
@@ -1,6 +1,23 @@
 ﻿namespace FilterBuilder
 
 open System
+open System.Threading
+open NBitcoin
 
 module Program =
     Console.WriteLine "generate filters"
+    open Filters
+    open NBitcoin.RPC
+    open Building
+
+    let network = Network.Main
+    let rpcClient = RPCClient(RPCCredentialString.Parse "****:****", network)
+    let initialData = taprootActivation network
+    let cts = new CancellationTokenSource()
+    let filterSaver = createFilterSaver ()
+    let s = filterSaver.Error.Subscribe (fun _ -> cts.Cancel())
+    let filterBuilder = createFilterBuilder filterSaver.Post
+
+    let filterCreationProcess = startBuilding rpcClient initialData.PrevBlockHash filterBuilder.Post
+    Async.RunSynchronously (filterCreationProcess, cancellationToken=cts.Token)
+    s.Dispose()
