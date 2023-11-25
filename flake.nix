b2bed2abf15c25012d8e96e77f6318db21157cdf
Lucas Ontivero <lucasontivero@gmail.com>
2023-11-25 17:32:45 -0300

    Organize flake a bit better

--- flake.nix
+++ flake.nix
@@ -6,89 +6,49 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        report-generator = pkgs.buildDotnetGlobalTool {
-          pname = "reportgenerator";
-          nugetName = "dotnet-reportgenerator-globaltool";
-          version = "5.1.23";
-          nugetSha256 = "sha256-zN/sUYRyeJnHLUsQtH7OTbCeeZ83ZD1SI3nAD/cZut4=";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      code-coverage-report = pkgs.callPackage ./coverage.nix {};
+      git-hooks = pkgs.callPackage ./trailing-spaces.nix {};
+    in {
+      packages = {
+        default = self.packages.${system}.berlino-wallet;
+
+        berlino-wallet = pkgs.callPackage ./default.nix {
+          dotnet-sdk = pkgs.dotnet-sdk_7;
+          dotnet-runtime = pkgs.dotnet-runtime_7;
         };
-        code-coverage-report = pkgs.writeShellScriptBin "coverage" ''
-          ${pkgs.dotnet-sdk_7}/bin/dotnet test --no-build --collect:"XPlat Code Coverage"
-          ${report-generator}/bin/reportgenerator reportgenerator -reports:./**/TestResults/*/coverage.cobertura.xml -targetdir:coveragereport -reporttypes:Html
-          rm -rf **/TestResults
-          $BROWSER coveragereport/index.html
-        '';
-        git-hooks = pkgs.writeShellScriptBin "pre-commit" ''
-          set -e
-          IFS="
-          "
+      };
 
-          for line in $(${pkgs.git}/bin/git diff --check --cached | sed '/^[+-]/d') ; do
-            FILE="$(echo "$line" | sed -r 's/:[0-9]+: .*//')"
-            mv -f "$FILE" "$FILE.save"
-            ${pkgs.git}/bin/git checkout -- "$FILE"
-            sed -i 's/[[:space:]]*$//' "$FILE"
-            ${pkgs.git}/bin/git add "$FILE"
-            sed 's/[[:space:]]*$//' "$FILE.save" > "$FILE"
-            rm "$FILE.save"
-          done
+      devShells = with pkgs; {
+        default = mkShell {
+          name = "berlino-shell";
+          packages = [
+            dotnet-sdk_7
+            nuget-to-nix
+            sqlite-interactive
+            bitcoin
+            tor
+            websocat
+            git-hooks
+            code-coverage-report
+          ];
 
-          if [ "--$(${pkgs.git}/bin/git status -s | grep '^[A|D|M]')--" = "----" ]; then
-            # empty commit
-            echo
-            echo -e "\033[31mNO CHANGES ADDED, ABORT COMMIT!\033[0m"
-            exit 1
-          fi
+          DOTNET_ROOT = "${dotnet-sdk_7}";
 
-          TEMP_DIRECTORY=$(mktemp -d)
-          SRC_DIRECTORY=$(pwd)
-
-          ${pkgs.git}/bin/git clone .git $TEMP_DIRECTORY
-          ${pkgs.git}/bin/git diff -P --cached | patch -p1 -d $TEMP_DIRECTORY
-          pushd $TEMP_DIRECTORY
-          ${pkgs.dotnet-sdk_7}/bin/dotnet test
-          popd
-          rm -rf $TEMP_DIRECTORY
+          shellHook = ''
+            export DOTNET_CLI_TELEMETRY_OPTOUT=1
+            export DOTNET_NOLOGO=1
+            export GIT_TOP_LEVEL="$(${pkgs.git}/bin/git rev-parse --show-toplevel)"
+            ln -f -s ${git-hooks}/bin/pre-commit $GIT_TOP_LEVEL/.git/hooks/pre-commit
+            export PS1='\n\[\033[1;34m\][Berlino:\w]\$\[\033[0m\] '
           '';
-      in {
-        packages = {
-          default = self.packages.${system}.berlino-wallet;
-
-          berlino-wallet = pkgs.callPackage ./default.nix {
-            dotnet-sdk = pkgs.dotnet-sdk_7;
-            dotnet-runtime = pkgs.dotnet-runtime_7;
-          };
         };
-
-        devShells = with pkgs; {
-          default = mkShell {
-            name = "berlino-shell";
-            packages = [
-              dotnet-sdk_7
-              nuget-to-nix
-              sqlite-interactive
-              bitcoin
-              tor
-              websocat
-              git-hooks
-              report-generator
-              code-coverage-report
-              ];
-
-            DOTNET_ROOT = "${dotnet-sdk_7}";
-
-            shellHook = ''
-              export DOTNET_CLI_TELEMETRY_OPTOUT=1
-              export DOTNET_NOLOGO=1
-              export GIT_TOP_LEVEL="$(${pkgs.git}/bin/git rev-parse --show-toplevel)"
-              ln -f -s ${git-hooks}/bin/pre-commit $GIT_TOP_LEVEL/.git/hooks/pre-commit
-              export PS1='\n\[\033[1;34m\][Berlino:\w]\$\[\033[0m\] '
-            '';
-         };
-        };
-      });
+      };
+    });
 }
